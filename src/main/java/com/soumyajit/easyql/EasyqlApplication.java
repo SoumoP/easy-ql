49448a8c58b8ac2af4ce5a149deda44e725283d9
Soumyajit Podder <poddersoumyajit@gmail.com>
2026-06-03 19:08:47 +0530

    chore: bootstrap Maven project with Spring Boot 3.3 and Java 21

---
+++ src/main/java/com/soumyajit/easyql/EasyqlApplication.java
@@ -0,0 +1,11 @@
+package com.soumyajit.easyql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyqlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyqlApplication.class, args);
+    }
+}
